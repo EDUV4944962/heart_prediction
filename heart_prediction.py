ed679fc008ecce8d72e78013b84b96ec460a3f1c
EDUV4944962 <eduv4944962@vossie.net>
2024-05-21 22:16:16 +0200

    Update heart_prediction.py

--- heart_prediction.py
+++ heart_prediction.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.exceptions import NotFittedError
+
 
 #Load the K-Nearest Neighbour Classifier model
 def load_model():
